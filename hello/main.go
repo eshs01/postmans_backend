aae5c762ee23476a7144695e8a1ee77418051176
eshs01 <f20241262@pilani.bits-pilani.ac.in>
2025-02-22 01:19:15 +0530

    second commit

--- hello/main.go
+++ hello/main.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	//Booleans, with boolean operators as you’d expect.
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
