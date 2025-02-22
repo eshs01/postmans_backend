f4a998589e4f88fcb8cfad694edde7ada87a92b4
eshs01 <f20241262@pilani.bits-pilani.ac.in>
2025-02-22 17:19:50 +0530

    constants learned

--- hello/main.go
+++ hello/main.go
@@ -1,17 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+const b string = "suii"
 
 func main() {
-	fmt.Println("hello")
-	// time for variables
-	var a int = 10
-	fmt.Println(a)
-	var b = "suckker"
 	fmt.Println(b)
-	d := "god"
-	c := 20
+	const n = 500000000
+
+	const d = 3e20 / n
 	fmt.Println(d)
-	fmt.Println(c)
+	fmt.Println(int64(d))
+
+	fmt.Println(math.Sin(n))
 
 }
