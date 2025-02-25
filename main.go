01a773be34920ff3c6ec0dc10b1350e8d329c7a3
eshs01 <f20241262@pilani.bits-pialni.ac.in>
2025-02-25 17:15:04 +0530

    commit one

--- main.go
+++ main.go
@@ -2,39 +2,56 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"strings"
 
 	"github.com/xuri/excelize/v2"
 )
 
-func main() {
-	f, err := excelize.OpenFile("Book1.xlsx")
-	if err != nil {
-		fmt.Println(err)
+// fuction to check errors
+func check(e error) {
+	if e != nil {
+		fmt.Println(e)
 		return
 	}
+
+}
+func isRowEmpty(row []string) bool {
+	for _, cell := range row {
+		if strings.TrimSpace(cell) != "" {
+			return false
+		}
+	}
+	return true
+}
+
+type data struct {
+	sc_no    []int
+	class_no []int
+}
+
+func main() {
+
+	dat, err := os.ReadFile("/tmp/dat")
+	check(err)
+
+	f, err := excelize.OpenFile(string(dat))
+	check(err)
 	defer func() {
-		// Close the spreadsheet.
+
 		if err := f.Close(); err != nil {
 			fmt.Println(err)
 		}
 	}()
-	// Get value from cell by given worksheet name and cell reference.
-	cell, err := f.GetCellValue("Sheet1", "B2")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(cell)
-	// Get all the rows in the Sheet1.
 	rows, err := f.GetRows("Sheet1")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for _, row := range rows {
-		for _, colCell := range row {
-			fmt.Print(colCell, "\t")
+	check(err)
+
+	for _, row := range rows { // this code was taken to skip the empty rows in aspreadsheet
+
+		if isRowEmpty(row) {
+			continue
 		}
-		fmt.Println()
+
 	}
 }
